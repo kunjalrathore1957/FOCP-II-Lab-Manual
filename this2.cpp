75d78b4f86d621018ca65306867a7810ab7c91b7
Kunjal Rathore <kunjalrathore1957@gmail.com>
2026-03-24 11:06:23 +0530

    Add files via upload

---
+++ this2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ using namespace std;
+
+class Person
+{
+    string name; int age;
+    public:
+    Person(){
+        cout<<"\nPerson default constructor"; name
+    }
+    
+}
